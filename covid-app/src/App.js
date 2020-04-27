2f1ff7192539d80a0c2a3290d479103b9427f9f7
maaz1996 <maaz.abdullah111@gmail.com>
2020-04-28 04:13:35 +0530

    api added; api call successful

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchdata } from "./api";
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchdata();
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
